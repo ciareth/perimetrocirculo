adb07e6b9a60748f32e3d98e6ffda197055af28a
damian <jiselsantos18@gmail.com>
2024-09-18 14:37:19 -0600

    Add files via upload

---
+++ ejercicio2.js
@@ -0,0 +1,5 @@
+// Calcular el perimetro de un circulo
+let r = prompt("Ingrese el valor del radio del circulo: ");
+p = (2*Math.PI)*r;
+
+document.write("El perimetro del circulo con un radio de ", r," es: ", p);
